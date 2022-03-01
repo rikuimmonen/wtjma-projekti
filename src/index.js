ef9a9436b97c2d9c8971fa714d970eb3de4e7b2e
Hetizz <heta.huostila@metropolia.fi>
2022-03-01 14:47:49 +0200

    Lunch branch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import HSLData from "./modules/hsl-data";
 import { fetchData } from "./modules/network";
 import utils from "./modules/utils";
 import weatherData from "./modules/weather-data";
+import SodexoData from './modules/sodexo-data';
+import FazerData from './modules/fazer-data';
 
 console.log(new Date());
 fetchData(HSLData.apiUrl, {
@@ -96,8 +98,44 @@ const getLocation = () => {
   }
 };
 
-
 getLocation();
+
+
+//Lunch menus
+
+const printMenu = (menu, targetId) => {
+  const ulElement = document.querySelector('#' + targetId);
+  ulElement.innerHTML = '';
+  for (const item of menu) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    ulElement.appendChild(li);
+  }
+};
+
+fetchData(SodexoData.dataUrl).then(data => {
+  const courses = SodexoData.createMenus(data.courses);
+  console.log('SODEXO COURSES', courses);
+  printMenu(courses, 'sodexoMenu');
+});
+
+
+const getDayIndex = () => {
+  let weekday = new Date().getDay() - 1;
+  // weekend no service, use fridays menu
+  if (weekday >= 5) {
+    weekday = 4;
+  } else if (weekday < 0) {
+    weekday = 4;
+  }
+  return weekday;
+};
+
+fetchData(FazerData.dataUrlFi, {},'fazer-php').then(data => {
+  const courses = FazerData.createDayMenu(data.LunchMenus, getDayIndex());
+  console.log('FAZER COURSES', courses);
+  printMenu(courses, 'fazerMenu');
+});
 
 
 
