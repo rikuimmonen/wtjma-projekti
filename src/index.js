30d97d588825afcc3dc7aee75937b0caa8545b69
Hetizz <heta.huostila@metropolia.fi>
2022-03-03 19:32:50 +0200

    Myllypuro lunch

--- src/index.js
+++ src/index.js
@@ -102,48 +102,7 @@ const getLocation = () => {
 getLocation();
 
 
-//Lunch menus
 
-const printMenu = (menu, targetId) => {
-  const ulElement = document.querySelector('#' + targetId);
-  ulElement.innerHTML = '';
-  for (const item of menu) {
-    const li = document.createElement('li');
-    li.textContent = item;
-    ulElement.appendChild(li);
-  }
-};
-
-//Sodexo Myyrmäki
-fetchData(SodexoData.dataUrl).then(data => {
-  const courses = SodexoData.createMenus(data.courses);
-  console.log('MYRTSI COURSES', courses);
-  printMenu(courses, 'sodexoMenu');
-});
-
-//Sodexo Myllypuro
-fetchData(MyllyData.dataUrl).then(data => {
-  const courses = MyllyData.createMenus(data.courses);
-  console.log('MYLLYPURO', courses);
-});
-
-const getDayIndex = () => {
-  let weekday = new Date().getDay() - 1;
-  // weekend no service, use fridays menu
-  if (weekday >= 5) {
-    weekday = 4;
-  } else if (weekday < 0) {
-    weekday = 4;
-  }
-  return weekday;
-};
-
-//Fazer Karaportti
-fetchData(FazerData.dataUrlFi, {},'fazer-php').then(data => {
-  const courses = FazerData.createDayMenu(data.LunchMenus, getDayIndex());
-  console.log('FAZER COURSES', courses);
-  printMenu(courses, 'fazerMenu');
-});
 
 
 
