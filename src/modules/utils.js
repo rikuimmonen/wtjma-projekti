5c089c0d48a1c5ec76aa56ffa80abbbc436b8780
Hetizz <heta.huostila@metropolia.fi>
2022-03-09 17:20:12 +0200

    Lunch 2

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -4,22 +4,73 @@ import {fetchData} from './network';
 import SodexoData from './sodexo-data';
 import MyllyData from './mylly-data';
 import FazerData from './fazer-data';
+import LuovaData from './luova-data';
 
-const printMenu = (menu, targetId) => {
-  const ulElement = document.querySelector('#' + targetId);
-  ulElement.innerHTML = '';
-  for (const item of menu) {
+const getDayIndex = () => {
+  let weekday = new Date().getDay() - 1;
+  // weekend no service, use fridays menu
+  if (weekday >= 5) {
+    weekday = 4;
+  } else if (weekday < 0) {
+    weekday = 4;
+  }
+  return weekday;
+};
+
+const printMenu = (menu) => {
+  const ul = document.querySelector('#lunch');
+  ul.innerHTML = '';
+  for (const course of menu) {
+    console.log(course);
     const li = document.createElement('li');
-    li.textContent = item;
-    ulElement.appendChild(li);
+    li.textContent = course;
+    ul.appendChild(li);
   }
 };
 
-//Sodexo Myyrmäki
+const getLunch = () => {
+  const storedCampus = JSON.parse(localStorage.getItem('location'));
+  console.log(storedCampus.nameFi);
+  const campusName = storedCampus.nameFi;
+  switch (campusName) {
+    case 'Arabian kampus':
+      fetchData(LuovaData.dataUrl, {}, 'allorigins').then(data => {
+        const courses = LuovaData.createMenus(data[getDayIndex()+1]);
+        console.log('LUOVA COURSES', courses);
+        printMenu(courses);
+      });
+      break;
+    case 'Karamalmin kampus':
+      fetchData(FazerData.dataUrlFi, {},'fazer-php').then(data => {
+        const courses = FazerData.createDayMenu(data.LunchMenus, getDayIndex());
+        console.log('FAZER COURSES', courses);
+        printMenu(courses);
+      });
+      break;
+    case 'Myllypuron kampus':
+      fetchData(MyllyData.dataUrl).then(data => {
+        const courses = MyllyData.createMenus(data.courses);
+        console.log('MYLLYPURO', courses);
+        printMenu(courses);
+      });
+      break;
+    case 'Myyrmäen kampus':
+      fetchData(SodexoData.dataUrl).then(data => {
+        const courses = SodexoData.createMenus(data.courses);
+        console.log('MYRTSI COURSES', courses);
+        printMenu(courses);
+      });
+      break;
+    default:
+      console.log('lol');
+  }
+};
+
+/*//Sodexo Myyrmäki
 fetchData(SodexoData.dataUrl).then(data => {
   const courses = SodexoData.createMenus(data.courses);
-  console.log('MYRTSI COURSES', courses);
-  printMenu(courses, 'sodexoMenu');
+  //console.log('MYRTSI COURSES', courses);
+  //printMenu(courses, 'sodexoMenu');
 });
 
 //Sodexo Myllypuro
@@ -43,8 +94,18 @@ const getDayIndex = () => {
 fetchData(FazerData.dataUrlFi, {},'fazer-php').then(data => {
   const courses = FazerData.createDayMenu(data.LunchMenus, getDayIndex());
   console.log('FAZER COURSES', courses);
-  printMenu(courses, 'fazerMenu');
+  //printMenu(courses, 'fazerMenu');
 });
+
+//Luova Arabia
+fetchData(LuovaData.dataUrl, {}, 'allorigins').then(data => {
+  //console.log('LUOVA DATA', data[getDayIndex()+1]);
+  const courses = LuovaData.createMenus(data[getDayIndex()+1]);
+  console.log('LUOVA COURSES', courses);
+  //printMenu(courses, 'luovaMenu');
+});*/
+
+getLunch();
 
 const utils = {};
 export default utils;
